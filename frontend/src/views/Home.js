5b3129c6403417f6ac05ceb1764671970f8c83ed
Yufa <112290188+01001101CK@users.noreply.github.com>
2023-10-17 20:12:24 -0700

    customized register/signin page and created guarded pages behind passage authentication

--- frontend/src/views/Home.js
+++ frontend/src/views/Home.js
@@ -3,12 +3,18 @@ import {
   PassageUnAuthGuard,
 } from "@passageidentity/passage-react";
 import { Navigate } from "react-router-dom";
+import React from "react";
+import styles from "../styles/Home.module.css";
 
 function Home() {
   return (
-    <PassageUnAuthGuard authComp={<Navigate to="/dashboard" />}>
-      <PassageAuth />
-    </PassageUnAuthGuard>
+    <div className={styles.wrapper}>
+    <div className={styles.mainContainer}>
+      <PassageUnAuthGuard authComp={<Navigate to="/dashboard" />}>
+        <PassageAuth />
+      </PassageUnAuthGuard>
+    </div>
+    </div >
   );
 }
 
