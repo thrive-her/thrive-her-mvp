14f5e79598c65deb82535ad9a846cdde2d71117a
Yufa <112290188+01001101CK@users.noreply.github.com>
2023-10-20 14:38:25 -0700

    updated CrisisTextLink link and updated profile page

--- frontend/src/views/Profile.js
+++ frontend/src/views/Profile.js
@@ -1,10 +1,23 @@
 import { PassageProfile } from "@passageidentity/passage-react";
+import { PassageAuthGuard } from "@passageidentity/passage-react";
+import styles from "../styles/Dashboard.module.css";
 
 function Profile() {
   return (
+    <PassageAuthGuard
+      unAuthComp={
+        <div className={styles.loginDashboard}>
+          <div className={styles.title}>You must be logged in</div>
+          <div className={styles.message}>
+            <a className={styles.login} href="/">Login</a>
+          </div>
+        </div>
+      }
+    >
     <div>
       <PassageProfile />
     </div>
+    </PassageAuthGuard>
   );
 }
 
